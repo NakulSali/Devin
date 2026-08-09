01468eef7211223a68af9ca16f16f2b492b3a6c9
nakul <nakulsali935@gmail.com>
2026-08-09 14:36:48 +0530

    update-3

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import connect from './db/db.js'
 import userRoutes from './routes/user.routes.js'
 import cookieParser from 'cookie-parser'
@@ -20,8 +22,13 @@ app.use('/users', userRoutes)
 app.use('/projects', projectRoutes)
 app.use('/ai', aiRoutes)
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 export default app
